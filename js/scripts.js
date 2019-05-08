e7fa05341f2237859f0c48ba859837c11f5cc845
Nebojsa Vulicevic <v.nele.z@gmail.com>
2019-05-08 17:38:28 +0200

    rad

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,30 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+$(document).ready(function(){
+    
+    
+    $('.menu-button').click(function(){
+        $(this).next().addClass('active');
+        $('.main-menu-overlay').show();
+    });
+    
+    $('.main-menu-overlay').click(function(){
+        $('.main-menu-wrapper').removeClass('active');
+        $(this).hide();
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});//**************************************************END**************************************************
